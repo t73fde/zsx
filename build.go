ecd003fc0cbe77cd112ffec1ff9d0fabb7146ab2
stern <ds@zettelstore.de>
2025-10-06 16:21:50 +0000

    Add some more SZ getter functions (hopefully all)
    FossilOrigin-Name: d27a93ba563a54182222f300e5cb51034265f939e9486124d5438fd9af29aefd

--- build.go
+++ build.go
@@ -28,15 +28,34 @@ func MakeBlock(blocks ...*sx.Pair) *sx.Pair {
 // MakeBlockList builds a block node from a list of blocks.
 func MakeBlockList(blocks *sx.Pair) *sx.Pair { return blocks.Cons(SymBlock) }
 
+// GetBlock returns all elements of a block node.
+func GetBlock(node *sx.Pair) *sx.Pair { return node.Tail() }
+
 // MakeInlineList builds an inline node from a list of inlines.
 func MakeInlineList(inlines *sx.Pair) *sx.Pair { return inlines.Cons(SymInline) }
+
+// GetInline returns all elements of an inline node.
+func GetInline(node *sx.Pair) *sx.Pair { return node.Tail() }
 
 // MakeParaList builds a paragraph node.
 func MakeParaList(inlines *sx.Pair) *sx.Pair { return inlines.Cons(SymPara) }
 
+// GetPara returns all elements of a paragraph node.
+func GetPara(node *sx.Pair) *sx.Pair { return node.Tail() }
+
 // MakeList builds a list node.
 func MakeList(sym *sx.Symbol, attrs *sx.Pair, items *sx.Pair) *sx.Pair {
 	return items.Cons(attrs).Cons(sym)
+}
+
+// GetList returns the elements of a list node.
+func GetList(node *sx.Pair) (*sx.Symbol, *sx.Pair, *sx.Pair) {
+	if sym, isSymbol := sx.GetSymbol(node.Car()); isSymbol {
+		attrsNode := node.Tail()
+		blocks := attrsNode.Tail()
+		return sym, attrsNode.Head(), blocks
+	}
+	return nil, nil, nil
 }
 
 // MakeVerbatim builds a node for verbatim text.
@@ -108,25 +127,45 @@ func MakeThematic(attrs *sx.Pair) *sx.Pair {
 // GetThematic returns the elements of a thematic break node.
 func GetThematic(node *sx.Pair) *sx.Pair { return node.Tail().Head() }
 
-// GetList returns the elements of a list node.
-func GetList(node *sx.Pair) (*sx.Symbol, *sx.Pair, *sx.Pair) {
-	if sym, isSymbol := sx.GetSymbol(node.Car()); isSymbol {
-		attrsNode := node.Tail()
-		blocks := attrsNode.Tail()
-		return sym, attrsNode.Head(), blocks
-	}
-	return nil, nil, nil
+// GetDescription returns the elements of a description list node.
+func GetDescription(node *sx.Pair) (*sx.Pair, *sx.Pair) {
+	attrsNode := node.Tail()
+	termsVals := attrsNode.Tail()
+	return attrsNode.Head(), termsVals
+}
+
+// GetTable returns the elements of a table.
+func GetTable(node *sx.Pair) (*sx.Pair, *sx.Pair, *sx.Pair) {
+	attrsNode := node.Tail()
+	headerNode := attrsNode.Tail()
+	rows := headerNode.Tail()
+	return attrsNode.Head(), headerNode.Head(), rows
 }
 
 // MakeCell builds a table cell node.
 func MakeCell(attrs *sx.Pair, inlines *sx.Pair) *sx.Pair { return inlines.Cons(attrs).Cons(SymCell) }
 
-// MakeTransclusion builds a transclusion node.
+// GetCell returns all elements of a table cell node.
+func GetCell(node *sx.Pair) (*sx.Pair, *sx.Pair) {
+	attrsNode := node.Tail()
+	inlines := attrsNode.Tail()
+	return attrsNode.Head(), inlines
+}
+
+// MakeTransclusion builds a transclude node.
 func MakeTransclusion(attrs *sx.Pair, ref *sx.Pair, text *sx.Pair) *sx.Pair {
 	if text == nil {
 		return sx.MakeList(SymTransclude, attrs, ref)
 	}
 	return text.Cons(ref).Cons(attrs).Cons(SymTransclude)
+}
+
+// GetTransclusion returns all elements of a tranclude node.
+func GetTransclusion(node *sx.Pair) (*sx.Pair, *sx.Pair, *sx.Pair) {
+	attrNode := node.Tail()
+	refNode := attrNode.Tail()
+	inlines := refNode.Tail()
+	return attrNode.Head(), refNode.Head(), inlines
 }
 
 // MakeBLOB builds a block BLOB node.
@@ -138,9 +177,31 @@ func MakeBLOB(attrs, description *sx.Pair, syntax, content string) *sx.Pair {
 					sx.Cons(sx.MakeString(content), sx.Nil())))))
 }
 
+// GetBLOB returns all elements of a block BLOB node.
+func GetBLOB(node *sx.Pair) (*sx.Pair, *sx.Pair, string, string) {
+	attrsNode := node.Tail()
+	descrNode := attrsNode.Tail()
+	syntaxNode := descrNode.Tail()
+	if syntax, isSyntax := sx.GetString(syntaxNode.Car()); isSyntax {
+		contentNode := syntaxNode.Tail()
+		if content, isContent := sx.GetString(contentNode.Car()); isContent {
+			return attrsNode.Head(), descrNode.Head(), syntax.GetValue(), content.GetValue()
+		}
+	}
+	return nil, nil, "", ""
+}
+
 // MakeText builds a text node.
 func MakeText(text string) *sx.Pair {
 	return sx.Cons(SymText, sx.Cons(sx.MakeString(text), sx.Nil()))
+}
+
+// GetText returns the element of a text node.
+func GetText(node *sx.Pair) string {
+	if s, isString := sx.GetString(node.Tail().Car()); isString {
+		return s.GetValue()
+	}
+	return ""
 }
 
 // MakeSoft builds a node for a soft line break.
@@ -185,9 +246,34 @@ func MakeEmbedBLOB(attrs *sx.Pair, syntax string, content string, inlines *sx.Pa
 	return inlines.Cons(sx.MakeString(content)).Cons(sx.MakeString(syntax)).Cons(attrs).Cons(SymEmbedBLOB)
 }
 
+// GetEmbedBLOB returns all elements of an inline BLOB node.
+func GetEmbedBLOB(node *sx.Pair) (*sx.Pair, string, string, *sx.Pair) {
+	attrsNode := node.Tail()
+	syntaxNode := attrsNode.Tail()
+	if syntax, isSyntax := sx.GetString(syntaxNode.Car()); isSyntax {
+		contentNode := syntaxNode.Tail()
+		if content, isContent := sx.GetString(contentNode.Car()); isContent {
+			inlines := contentNode.Tail()
+			return attrsNode.Head(), syntax.GetValue(), content.GetValue(), inlines
+		}
+	}
+	return nil, "", "", nil
+}
+
 // MakeCite builds a node that specifies a citation.
 func MakeCite(attrs *sx.Pair, text string, inlines *sx.Pair) *sx.Pair {
 	return inlines.Cons(sx.MakeString(text)).Cons(attrs).Cons(SymCite)
+}
+
+// GetCite returns alle elements of a cite node.
+func GetCite(node *sx.Pair) (*sx.Pair, string, *sx.Pair) {
+	attrsNode := node.Tail()
+	keyNode := attrsNode.Tail()
+	if key, isString := sx.GetString(keyNode.Car()); isString {
+		inlines := keyNode.Tail()
+		return attrsNode.Head(), key.GetValue(), inlines
+	}
+	return nil, "", nil
 }
 
 // MakeEndnote builds an endnote node.
@@ -195,9 +281,32 @@ func MakeEndnote(attrs, inlines *sx.Pair) *sx.Pair {
 	return inlines.Cons(attrs).Cons(SymEndnote)
 }
 
+// GetEndnote returns the elements of an endnote node.
+func GetEndnote(node *sx.Pair) (*sx.Pair, *sx.Pair) {
+	attrsNode := node.Tail()
+	inlines := attrsNode.Tail()
+	return attrsNode.Head(), inlines
+}
+
 // MakeMark builds a mark note.
 func MakeMark(mark string, slug, fragment string, inlines *sx.Pair) *sx.Pair {
 	return inlines.Cons(sx.MakeString(fragment)).Cons(sx.MakeString(slug)).Cons(sx.MakeString(mark)).Cons(SymMark)
+}
+
+// GetMark returns the elements of a mark node.
+func GetMark(node *sx.Pair) (string, string, string, *sx.Pair) {
+	markNode := node.Tail()
+	if mark, isMark := sx.GetString(markNode.Car()); isMark {
+		slugNode := markNode.Tail()
+		if slug, isSlug := sx.GetString(slugNode.Car()); isSlug {
+			fragmentNode := slugNode.Tail()
+			if fragment, isFragment := sx.GetString(fragmentNode.Car()); isFragment {
+				inlines := fragmentNode.Tail()
+				return mark.GetValue(), slug.GetValue(), fragment.GetValue(), inlines
+			}
+		}
+	}
+	return "", "", "", nil
 }
 
 // MakeFormat builds an inline formatting node.
